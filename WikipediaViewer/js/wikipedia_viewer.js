887f809b2a4c607bf4b4a1aab987a5d108cf4664
Coder Klemen <coder.klemen@gmail.com>
2018-07-25 21:51:42 +0200

    tried to fix WikipediaViewer..slowly getting  there...

--- WikipediaViewer/js/wikipedia_viewer.js
+++ WikipediaViewer/js/wikipedia_viewer.js
@@ -1,27 +1,49 @@
 $(document).ready(function() {
-
-    // let search = $("#usr").html();
   
     $("#target").submit( function(event) {
     
       event.preventDefault();
       
       let $form = $(this),
-          search = $form.find("input[name='inp']").val();
+          search = $form.find("#inp").val();
+
+        $.ajax({
+          url: 'http://anyorigin.com/get?url=' +
+                "http://en.wikipedia.org/w/api.php",
+          data: {
+            action: "query",
+            list: "search",
+            srsearch: search,
+            srlimit: 10,
+            //format: "json",
+            origin: '*'
+          },
+          dataType: "jsonp",
+        complete: function(jqXHR, status) {
+          console.log("here i am");
+          parser=new DOMParser();
+          htmlDoc=parser.parseFromString(jqXHR, "text/html");
+          console.log(htmlDoc);
+          // console.log(status);
+          // console.log(jqXHR);
           
-      
-      $.ajax({
-        url: "//en.wikipedia.org/w/api.php",
-        data: {
-          action: "query",
-          list: "search",
-          srsearch: search,
-          srlimit: 10,
-          format: "json"
+          for (let i = 0, x = 10; i < x; i++) {
+            $("#p"+ i +"_page").prop("href", "https://en.wikipedia.org/?curid=" + jsonData.query.search[i].pageid);
+            $("#p"+ i +"_page").html(jsonData.query.search[i].title);
+            $("#p"+ i +"_content").html(jsonData.query.search[i].snippet);
+          }
+          $(".card").show();
+          $(".result").show();
+          $("#inp").val(''); 
         },
-        dataType: "jsonp"
-      })
-        .done(function(jsonData) {
+        
+        /* error: function(xhr, status, errorThrown) {
+          console.log("Error: " + errorThrown);
+          console.log("Status: " + status);
+          console.log("AJAX error: " + JSON.stringify(xhr));
+        }*/
+      })    
+        /*.done(function(jsonData) {
           for (let i = 0, x = 10; i < x; i++) {
             $("#p"+ i +"_page").prop("href", "https://en.wikipedia.org/?curid=" + jsonData.query.search[i].pageid);
             $("#p"+ i +"_page").html(jsonData.query.search[i].title);
@@ -38,9 +60,10 @@ $(document).ready(function() {
         })
         .always(function() {
           console.log("the request is complete");
-        });
+        });*/
+        
     });
-    
+
     $("#searchclear").click(function(){
       for (let i = 0, x = 10; i < x; i++) {
             $("#p"+ i +"_page").prop("href", "");
